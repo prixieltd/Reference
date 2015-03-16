9f5141533f534718281b69960bff14321507d20f
Simon Zsolt <simon.zsolt@datanetsystems.hu>
2015-03-16 15:50:12 +0100

    references

--- paging.php
+++ paging.php
@@ -12,39 +12,46 @@ class Paging{
 	 * UNIQUE id
 	 * 
 	 * @var int
+   * @access private     
 	 */
-  var $uid = 0;
-  /**
-   * All record number
-   * 
-   * @var int
-   */
-  var $rownum;
-  /**
-   * Record per page
-   * 
-   * @var int
-   */
-  var $rowperpage;
-  /**
-   * Page number
-   * 
-   * @var int 
-   */
-  var $pagenum;
-  /**
-   * Actual page
-   * 
-   * @var int
-   */
-  var $current_page;
+  private $uid = 0;
   /**
    * Use SESSION cache
    * 
    * @var bool
+   * @access private   
    */
-  var $storepagenum;
+  private $storepagenum;
+  /**
+   * All record number
+   * 
+   * @var int
+   * @access public     
+   */
+  public $rownum;
+  /**
+   * Record per page
+   * 
+   * @var int
+   * @access public    
+   */
+  public $rowperpage;
+  /**
+   * Page number
+   * 
+   * @var int 
+   * @access public      
+   */
+  public $pagenum;
+  /**
+   * Actual page
+   * 
+   * @var int
+   * @access public    
+   */
+  public $current_page;
 
+                     
   /**
    * Constructor
    *     
@@ -52,10 +59,10 @@ class Paging{
    * @param $rownum
    * @param $rowsperpage
    * @param bool storepagenum
-   * @return void
+   * @return void                         
    */
-  function Paging($uid, $rownum = 0, $rowsperpage = 10, $storepagenum = false){
-    $this->uid = $uid;
+  public function __construct($uid, $rownum = 0, $rowsperpage = 10, $storepagenum = false){
+    $this->uid = $uid;                                                                                 
     $this->rownum = $rownum;
     $this->rowsperpage = $rowsperpage;
   	$this->pagenum = (int)ceil($rownum/$rowsperpage);
@@ -77,7 +84,7 @@ class Paging{
    * 
    * @return bool
    */
-  function nextPage(){
+  public function nextPage(){
 		if ($this->current_page < $this->pagenum){
 			$this->current_page++;
 			if ($this->storepagenum) $_SESSION['paging_'.$this->uid]['current_page'] = $this->current_page;
@@ -91,7 +98,7 @@ class Paging{
    * 
    * @return bool
    */
-  function prevPage(){
+  public function prevPage(){
 		if ($this->current_page > 1){
 			$this->current_page--;
 			if ($this->storepagenum) $_SESSION['paging_'.$this->uid]['current_page'] = $this->current_page;
@@ -107,7 +114,7 @@ class Paging{
    * @param $page Oldalszám
    * @return void
    */
-  function goToPage($page){
+  public function goToPage($page){
 		if ($page >= 1 && $page <= $this->pagenum){
 			$this->current_page = $page;
 			if ($this->storepagenum) $_SESSION['paging_'.$this->uid]['current_page'] = $this->current_page;
@@ -119,7 +126,7 @@ class Paging{
    * 
    * @return int
    */
-  function getRowsPerPage(){
+  public function getRowsPerPage(){
     return $this->rowsperpage;
   }
 
@@ -128,7 +135,7 @@ class Paging{
    * 
    * @return int
    */
-  function getFrom(){
+  public function getFrom(){
     $ret = ($this->current_page-1)*$this->rowsperpage;
     return ($ret<0)?0:$ret;
   }
@@ -138,7 +145,7 @@ class Paging{
    * 
    * @return int
    */
-  function getTo(){
+  public function getTo(){
     $return = ($this->current_page)*$this->rowsperpage-1;
     if ($return > $this->rownum) $return = $this->rownum;
     return $return;
@@ -149,10 +156,9 @@ class Paging{
    * 
    * @return int
    */
-  function getItemNum(){
+  public function getItemNum(){
     return $this->getTo()-$this->getFrom()+1;
   }
 
 }
 
-?>
